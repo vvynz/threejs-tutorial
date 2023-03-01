4e7e762424760284b1441f9e995a2e8a2beb8a65
Vivian Zhang <vvynz12@gmail.com>
2023-03-01 18:41:05 -0500

    Set up the scene, camera and renderer

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
-import './style.css'
+import "./style.css";
 
+import * as THREE from "three";
+
+// scene is like a container that holds all your objects, cameras and lights
+const scene = new THREE.Scene();
+
+// perspective camera is the most commonly used. It mimics what the human eye sees
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
