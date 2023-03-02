16945487f4b6c383298812d5c1e85c9dd9362fda
Vivian Zhang <vvynz12@gmail.com>
2023-03-01 20:36:32 -0500

    Create a moon shape and map textures over it

--- main.js
+++ main.js
@@ -93,3 +93,18 @@ function animate() {
 }
 
 animate();
+
+// Moon
+
+const moonTexture = new THREE.TextureLoader().load("/assets/moon.jpeg");
+const normalTexture = new THREE.TextureLoader().load("/assets/normal.jpeg");
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: moonTexture,
+    normalMap: normalTexture
+  })
+)
+
+scene.add(moon);
